2993cab24d52aab10d3372136ec86433ba0ae429
SerajKH <142510557+LozAlk@users.noreply.github.com>
2023-10-10 17:58:50 +0200

    Now Lesseon

---
+++ # 098_Installing And Use Pylint For Better Code .py
@@ -0,0 +1,11 @@
+# -----------------------------------------------
+# -- Installing And Use Pylint For Better Code --
+# -----------------------------------------------
+
+
+def syeHello(name):
+    msg ="Hello Python"
+
+    return f"{msg} {name}"
+
+syeHello("Alkherat")
