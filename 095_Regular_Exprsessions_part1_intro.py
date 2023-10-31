b2ff759cd5bf784e0e82219939a12d934b5406ad
SerajKH <142510557+LozAlk@users.noreply.github.com>
2023-10-31 03:46:54 +0100

    New lessons

---
+++ 095_Regular_Exprsessions_part1_intro.py
@@ -0,0 +1,13 @@
+# ----------------------------------
+# -- Regular Expressions => Intro --
+# ----------------------------------
+# [1] Sequence of Characters That Define A Search Pattern 
+# [2] Regular Expression is Not In Python Its General Concept
+# [3] Used In [Credit Card Validation, Ip Address Validation , Email Validation ]
+# [4] Test RegEx "https://pythex.org" # الدخول الئ هاذا الموقع
+# [5] Characters Sheet "https://www.debuggex.com/cheat sheet/regex/python"
+# --------------------------------------------------------------------------------
+
+
+
+# كلام نضري 
