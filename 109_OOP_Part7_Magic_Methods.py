9216ccf5b9720e985f6bd6133d2cf5e4ce5e1752
SerajKH <142510557+LozAlk@users.noreply.github.com>
2023-11-26 23:58:30 +0100

    Now File 109_OOP_Part7_Magic_Methods.py

---
+++ 109_OOP_Part7_Magic_Methods.py
@@ -0,0 +1,44 @@
+# -----------------------------------------------
+# -- Object Oriented Programming => magic Methods
+# -----------------------------------------------
+# Everything in Python Is An Object
+# __init__ Called Automatically When Instantuating Class
+# self.__class__ The class To Which a class instance Belongs
+# __str__ Gives a Human-Redable Output of the Object
+# __len__ Returns the Length of the Container
+#                        Called When we Use the Built-in len() Function on the Object
+# -----------------------------------------------------------------------------
+
+
+class Skill:
+    def __init__(self):
+        self.skills = ['Html','js','Css']
+
+    def __str__(self):
+        return f'This is My Skills I Have {self.skills}'
+
+    def __len__(self):
+        return len(self.skills)
+
+
+
+
+
+profile = Skill()
+#
+#print(profile)
+#print(len(profile))
+
+profile.skills.append('PHP')
+profile.skills.append('MySQL')
+
+print(profile)
+print(len(profile))
+#print(profile.__class__)
+        
+#my_sting = 'Seraj'
+#print(type(my_sting))
+#print(my_sting.__class__)
+#print(dir(str))
+
+#print(str.upper(my_sting))
